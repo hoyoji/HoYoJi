d7530776a93aaed6313baefaa39e7e641953ed64
Ben Yiu <szepan.yiu@gmail.com>
2013-03-15 19:32:31 +0800

    merge

--- app/controllers/user/userRow.js
+++ app/controllers/user/userRow.js
@@ -1,8 +1,8 @@
 Alloy.Globals.extendsBaseRowController($, arguments[0]);
 
-$.onWindowOpenDo(function(){
-	$.$model.on("change", function(){
-		$.userName.setText($.$model.xGet("userName"));
-	});
-	$.userName.setText($.$model.xGet("userName"));
-});
+// $.onWindowOpenDo(function(){
+	// $.$model.on("change", function(){
+		// $.userName.setText($.$model.xGet("userName"));
+	// });
+	// $.userName.setText($.$model.xGet("userName"));
+// });
