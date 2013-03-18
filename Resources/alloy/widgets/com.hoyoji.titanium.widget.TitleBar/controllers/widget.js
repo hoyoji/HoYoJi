77c13269db5d77995f55a41117c520eed03c9960
unknown <hoyoji_ZhangJingshan@.(none)>
2013-03-18 11:07:16 +0800

    11

--- Resources/alloy/widgets/com.hoyoji.titanium.widget.TitleBar/controllers/widget.js
+++ Resources/alloy/widgets/com.hoyoji.titanium.widget.TitleBar/controllers/widget.js
@@ -79,7 +79,7 @@ function Controller() {
         boundXTable = xTable;
         $.onWindowOpenDo(function() {
             if ($.getCurrentWindow().$attrs.selectorCallback) {
-                $.$attrs.title = "选择顶级" + $.$attrs.title;
+                $.$attrs.title = "选择" + $.$attrs.title + "(顶级)";
                 $.title.setText($.$attrs.title);
                 $.title.addEventListener("singletap", function(e) {
                     e.cancelBubble = !0;
