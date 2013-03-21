8dfdd84bb34543e4baf3f42bfb29dcd7b502cf41
bruselee <hoyoji.lijianqiang@gmail.com>
2013-03-21 21:15:36 +0800

    messageBox

--- app/models/MessageBox.js
+++ app/models/MessageBox.js
@@ -22,56 +22,55 @@ exports.definition = {
 			validators : {
 			},
 			processNewMessages : function(){
-				this.xGet("messages").map(function(msg){
-					console.info("------------=============================="+msg.xGet("messageState"));
-					if(msg.xGet("messageState") === "new"){
-						if(msg.xGet("type") === "System.Friend.AddResponse"){
-								msg.xSet("messageState","read");
-								var friendlength = Alloy.createCollection("Friend").xSearchInDb({
-								friendUserId : msg.xGet("fromUserId"),
-								ownerUserId : Alloy.Models.User.id
-								}).length;
-							if (friendlength===0) {
-								var friend = Alloy.createModel("Friend", {
-									ownerUser :　Alloy.Models.User,
-									friendUser : msg.xGet("fromUser"),
-									friendCategory : Alloy.Models.User.xGet("defaultFriendCategory")
-								});
-								friend.xSave();
-								console.info("------------====================-----------"+msg.xGet("fromUser").xGet("userName"));
-							}
-							msg.xSave();
-						}else if(msg.xGet("type") === "System.Friend.AutoAdd"){
+				var newMessages = Alloy.createCollection("Message").xSearchInDb({
+					messageBoxId : Alloy.Models.User.xGet("messageBoxId"),
+					toUserId : Alloy.Models.User.id,
+					messageState : "new"
+				})
+				newMessages.map(function(msg){
+					if(msg.xGet("type") === "System.Friend.AddResponse"){
 							msg.xSet("messageState","read");
 							var friendlength = Alloy.createCollection("Friend").xSearchInDb({
-								friendUserId : msg.xGet("fromUserId"),
-								ownerUserId : Alloy.Models.User.id
-								}).length;
-							if (friendlength===0) {
-								var friend = Alloy.createModel("Friend", {
-									ownerUser :　Alloy.Models.User,
-									friendUser : msg.xGet("fromUser"),
-									friendCategory : Alloy.Models.User.xGet("defaultFriendCategory")
-								});
-								friend.xSave();
-								console.info("------------====================-----------"+msg.xGet("fromUser").xGet("userName"));
-							}
-							msg.xSave();
-						}else if(msg.xGet("type") === "System.Friend.Delete"){
-							msg.xSet("messageState","read");
-							var friends = Alloy.createCollection("Friend").xSearchInDb({
-								friendUserId : msg.xGet("fromUserId"),
-								ownerUserId : Alloy.Models.User.id
-								});
-						    var friendlength = friends.length;
-							if (friendlength>0) {
-								// console.info("--------------*******------------------"+friends.at(0).xGet("friendUser").xGet("userName"));
-								friends.at(0)._xDelete(function(){
-									
-								});
-							}
-							msg.xSave();
+							friendUserId : msg.xGet("fromUserId"),
+							ownerUserId : Alloy.Models.User.id
+							}).length;
+						if (friendlength===0) {
+							var friend = Alloy.createModel("Friend", {
+								ownerUser :　Alloy.Models.User,
+								friendUser : msg.xGet("fromUser"),
+								friendCategory : Alloy.Models.User.xGet("defaultFriendCategory")
+							});
+							friend.xSave();
 						}
+						msg.xSave();
+					}else if(msg.xGet("type") === "System.Friend.AutoAdd"){
+						msg.xSet("messageState","read");
+						var friendlength = Alloy.createCollection("Friend").xSearchInDb({
+							friendUserId : msg.xGet("fromUserId"),
+							ownerUserId : Alloy.Models.User.id
+							}).length;
+						if (friendlength===0) {
+							var friend = Alloy.createModel("Friend", {
+								ownerUser :　Alloy.Models.User,
+								friendUser : msg.xGet("fromUser"),
+								friendCategory : Alloy.Models.User.xGet("defaultFriendCategory")
+							});
+							friend.xSave();
+						}
+						msg.xSave();
+					}else if(msg.xGet("type") === "System.Friend.Delete"){
+						msg.xSet("messageState","read");
+						var friends = Alloy.createCollection("Friend").xSearchInDb({
+							friendUserId : msg.xGet("fromUserId"),
+							ownerUserId : Alloy.Models.User.id
+							});
+					    var friendlength = friends.length;
+						if (friendlength>0) {
+							friends.at(0)._xDelete(function(){
+								
+							});
+						}
+						msg.xSave();
 					}
 				});
 			}
