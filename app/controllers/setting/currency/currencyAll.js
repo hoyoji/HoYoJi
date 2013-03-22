4e7cbe2e1d44cf61902cfa572d812364bf25a9a3
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-03-22 10:55:01 +0800

    3.22

--- app/controllers/setting/currency/currencyAll.js
+++ app/controllers/setting/currency/currencyAll.js
@@ -1,6 +1,6 @@
 Alloy.Globals.extendsBaseViewController($, arguments[0]);
 
-$.makeContextMenu = function() {
+$.makeContextMenu = function(e, isSelectMode, sourceModel) {
 	var menuSection = Ti.UI.createTableViewSection();
 	menuSection.add($.createContextMenuItem("新增币种", function() {
 		Alloy.Globals.openWindow("setting/currency/currencyForm");
