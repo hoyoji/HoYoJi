10eddcc47aad7d81d9ff12c87a4399eac0db6725
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-03-23 22:05:28 +0800

    edit to change category when project change

--- app/controllers/money/moneyIncomeForm.js
+++ app/controllers/money/moneyIncomeForm.js
@@ -11,22 +11,22 @@ if (!$.$model) {
 		incomeType : "Ordinary",
 		moneyAccount : Alloy.Models.User.xGet("activeMoneyAccount"),
 		project : Alloy.Models.User.xGet("activeProject"),
-		category : Alloy.Models.User.xGet("activeProject").xGet("defaultIncomeCategory")
+		moneyIncomeCategory : Alloy.Models.User.xGet("activeProject").xGet("defaultIncomeCategory")
 	});
 	$.setSaveableMode("add");
 }
 oldMoneyAccount = $.$model.xGet("moneyAccount");
 oldAmount = $.$model.xGet("amount");
 
-function setExchangeRate() {
+$.moneyAccount.field.addEventListener("change", function() {//根据账户的币种改变汇率
 	if ($.moneyAccount.getValue()) {
 		var exchangeCurrencyRateValue;
 		if ($.moneyAccount.getValue().xGet("currency") === $.$model.xGet("localCurrency")) {
 			console.info("+++++++++++++++++++++++++++++++++++hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			exchangeCurrencyRateValue = 1;
 		} else {
-			var exchanges =  $.$model.xGet("localCurrency").getExchanges($.moneyAccount.getValue().xGet("currency"));
-			if (exchanges) {
+			var exchanges = $.$model.xGet("localCurrency").getExchanges($.moneyAccount.getValue().xGet("currency"));
+			if (exchanges.length > 0) {
 				exchangeCurrencyRateValue = exchanges.at(0).xGet("rate");
 				console.info("++++++++++++++++++++++++++++++++" + exchanges.at(0).xGet("rate") + "+++hello2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			} else {
@@ -37,15 +37,14 @@ function setExchangeRate() {
 		$.exchangeCurrencyRate.setValue(exchangeCurrencyRateValue);
 		$.exchangeCurrencyRate.field.fireEvent("change");
 	}
-}
-
-$.moneyAccount.field.addEventListener("change", setExchangeRate);
-$.onWindowCloseDo(function() {
-	$.moneyAccount.field.removeEventListener("change", setExchangeRate);
 });
 
-$.onWindowOpenDo(function() {
-	setExchangeRate();
+$.project.field.addEventListener("change", function() {//项目改变，分类为项目的默认分类
+if($.project.getValue()){
+	var defaultIncomeCategory = $.project.getValue().xGet("defaultIncomeCategory");
+	$.moneyIncomeCategory.setValue(defaultIncomeCategory);
+	$.moneyIncomeCategory.field.fireEvent("change");
+}
 });
 
 $.onSave = function(saveEndCB, saveErrorCB) {
@@ -54,15 +53,17 @@ $.onSave = function(saveEndCB, saveErrorCB) {
 	var newAmount = $.$model.xGet("amount");
 	var oldCurrentBalance = oldMoneyAccount.xGet("currentBalance");
 
-	if (oldMoneyAccount.xGet("id") === newMoneyAccount.xGet("id")) {
+	if (oldMoneyAccount.xGet("id") === newMoneyAccount.xGet("id")) {//账户相同时，即新增和账户不改变的修改
 		newMoneyAccount.xSet("currentBalance", newCurrentBalance - oldAmount + newAmount);
-	} else {
+	} else {//账户改变时
 		oldMoneyAccount.xSet("currentBalance", oldCurrentBalance - oldAmount);
 		newMoneyAccount.xSet("currentBalance", newCurrentBalance + newAmount);
 	}
 
 	Alloy.Models.User.xSet("activeMoneyAccount", $.$model.xGet("moneyAccount"));
+	//记住当前账户为下次打开时的默认账户
 	Alloy.Models.User.xGet("activeProject").xSet("defaultIncomeCategory", $.$model.xGet("category"));
+	//记住当前分类为下次打开时的默认分类
 
 	$.saveModel(saveEndCB, function(e) {
 		newMoneyAccount.xSet("currentBalance", newMoneyAccount.previous("currentBalance"));
