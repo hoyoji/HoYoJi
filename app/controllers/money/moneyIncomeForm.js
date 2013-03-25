88cbd368b048fbadd2e4cdbb59430e4864160387
Ben Yiu <szepan.yiu@gmail.com>
2013-03-25 19:28:41 +0800

    moneyIncomeForm

--- app/controllers/money/moneyIncomeForm.js
+++ app/controllers/money/moneyIncomeForm.js
@@ -79,13 +79,15 @@ $.onSave = function(saveEndCB, saveErrorCB) {
 	if($.$model.isNew()){
 		//记住当前账户为下次打开时的默认账户
 		Alloy.Models.User.xSet("activeMoneyAccount", $.$model.xGet("moneyAccount"));
+		Alloy.Models.User.xSet("activeProject", $.$model.xGet("project"));
 		//记住当前分类为下次打开时的默认分类
-		Alloy.Models.User.xGet("activeProject").xSet("defaultIncomeCategory", $.$model.xGet("category"));
-		//直接把activeMoneyAccountId保存到数据库，不经过validation
-		Alloy.Models.User.save({
-				activeMoneyAccountId : $.$model.xGet("moneyAccount").xGet("id")
-		}, {patch : true, wait : true});
+		Alloy.Models.User.xGet("activeProject").xSet("defaultIncomeCategory", $.$model.xGet("moneyIncomeCategory"));
 		Alloy.Models.User.xGet("activeProject").xAddToSave($);
+		//直接把activeMoneyAccountId保存到数据库，不经过validation，注意用 {patch : true, wait : true}
+		Alloy.Models.User.save({
+				activeMoneyAccountId : $.$model.xGet("moneyAccount").xGet("id"),
+				activeProjectId : $.$model.xGet("project").xGet("id")
+		}, {patch : true, wait : true});
 	}
 	
 	if (isRateExist === false) {//若汇率不存在 ，保存时自动新建一条
@@ -100,8 +102,11 @@ $.onSave = function(saveEndCB, saveErrorCB) {
 	$.saveModel(saveEndCB, function(e) {
 		newMoneyAccount.xSet("currentBalance", newMoneyAccount.previous("currentBalance"));
 		oldMoneyAccount.xSet("currentBalance", oldMoneyAccount.previous("currentBalance"));
-		Alloy.Models.User.xSet("activeMoneyAccount", Alloy.Models.User.previous("moneyAccount"));
-		Alloy.Models.User.xGet("activeProject").xSet("defaultIncomeCategory", Alloy.Models.User.previous("activeProject").xGet("defaultIncomeCategory"));
+		if($.$model.isNew()){
+			Alloy.Models.User.xSet("activeMoneyAccount", Alloy.Models.User.previous("moneyAccount"));
+			Alloy.Models.User.xSet("activeProject", Alloy.Models.User.previous("activeProject"));
+			Alloy.Models.User.xGet("activeProject").xSet("defaultIncomeCategory", Alloy.Models.User.previous("activeProject").xGet("defaultIncomeCategory"));
+		}
 		saveErrorCB(e);
 	});
 }
