d9c47c1651647e4e68208abfa17607726395cf46
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-03-28 17:42:11 +0800

    edit incomedetail

--- app/controllers/money/moneyIncomeDetailForm.js
+++ app/controllers/money/moneyIncomeDetailForm.js
@@ -20,21 +20,27 @@ $.onSave = function(saveEndCB, saveErrorCB) {
 		var newAmount = $.$model.xGet("amount");
 		//detail的新值
 
+		var oldIncomeAmount = 0;
+		if ($.$model.xGet("moneyIncome").xGet("moneyIncomeDetails").length < 0) {//没有details时，新增detail前Income的amount，计算currentBalance时要先加上
+			oldIncomeAmount = $.$model.xGet("moneyIncome").previous("amount");
+		}
+
 		if (newMoneyAccount) {//从IncomeForm打开detailAll 传进newMoneyAccount
 			var newMoneyAccount = $.$model.xGet("moneyAccount").xAddToSave($);
 			var newCurrentBalance = newMoneyAccount.xGet("currentBalance");
 			if (oldMoneyAccount.xGet("id") === newMoneyAccount.xGet("id")) {//moneyAccount not change
-				newMoneyAccount.xSet("currentBalance", newCurrentBalance - oldAmount + newAmount);
+				newMoneyAccount.xSet("currentBalance", newCurrentBalance - oldIncomeAmount - oldAmount + newAmount);
 			} else {
 				oldMoneyAccount.xSet("currentBalance", oldCurrentBalance - oldAmount);
 				oldMoneyAccount.xAddToSave($);
 				newMoneyAccount.xSet("currentBalance", newCurrentBalance + newAmount);
 			}
 		} else {//从IncomeRow进入detailAll
+
 			if ($.$model.isNew) {//新增detail
-				var oldIncomeAmount = $.$model.xGet("moneyExpense").xGet("amount");
-				if ($.$model.xGet("moneyExpense").hasChanged("amount")) {
-					oldIncomeAmount = $.$model.xGet("moneyExpense").previous("amount");
+				var oldIncomeAmount = $.$model.xGet("moneyIncome").xGet("amount");
+				if ($.$model.xGet("moneyIncome").hasChanged("amount")) {
+					oldIncomeAmount = $.$model.xGet("moneyIncome").previous("amount");
 				}
 				oldMoneyAccount.xSet("currentBalance", oldCurrentBalance - oldIncomeAmount + newAmount);
 			} else {//修改detail
