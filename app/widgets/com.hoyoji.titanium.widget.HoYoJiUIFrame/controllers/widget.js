843d4bfd7c3b218d3c7d20162308fb5a1deb0572
Ben Yiu <szepan.yiu@gmail.com>
2013-04-17 01:33:24 +0800

    c

---
+++ app/widgets/com.hoyoji.titanium.widget.HoYoJiUIFrame/controllers/widget.js
@@ -0,0 +1,26 @@
+var attrs = arguments[0] || {};
+if(attrs.hasFooter === "false"){
+	$.footerBar.hide();
+	$.footerBar.setTop("100%");
+}
+
+if(attrs.hasHeader === "false"){
+	$.headerBar.hide();
+	$.headerBar.setTop("100%");
+}
+
+exports.showHeader = function(){
+	$.headerBar.show();
+}
+
+exports.hideHeader = function(){
+	$.headerBar.hide();
+}
+
+exports.showFooter = function(){
+	$.footerBar.show();
+}
+
+exports.hideFooter = function(){
+	$.footerBar.hide();
+}
