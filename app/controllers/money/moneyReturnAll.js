11813459a3f1cf70a17bea3907e4e4cb39bb9795
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-04-18 18:04:24 +0800

    fix #213

--- app/controllers/money/moneyReturnAll.js
+++ app/controllers/money/moneyReturnAll.js
@@ -5,7 +5,9 @@ var selectedBorrow = $.$attrs.selectedBorrow;
 $.makeContextMenu = function(e, isSelectMode, sourceModel) {
 	var menuSection = Ti.UI.createTableViewSection();
 	menuSection.add($.createContextMenuItem("新增还款", function() {
-		Alloy.Globals.openWindow("money/moneyReturnForm",{selectedBorrow : selectedBorrow});
+		Alloy.Globals.openWindow("money/moneyReturnForm", {
+			selectedBorrow : selectedBorrow
+		});
 	}));
 	return menuSection;
 }
@@ -16,3 +18,11 @@ var moneyReturns = selectedBorrow.xGet("moneyReturns");
 var interests = selectedBorrow.xGet("moneyReturns");
 $.moneyReturnsTable.addCollection(moneyReturns, "money/moneyReturnRow");
 $.moneyReturnsTable.addCollection(interests, "money/moneyReturnInterestRow");
+
+function onFooterbarTap(e) {
+	if (e.source.id === "addMoneyReturn") {
+		Alloy.Globals.openWindow("money/moneyReturnForm", {
+			selectedBorrow : selectedBorrow
+		});
+	}
+}
