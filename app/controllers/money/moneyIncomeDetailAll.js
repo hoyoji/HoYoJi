48b09de1aba0c248a741b973cee07ea437a35668
bruselee <hoyoji.lijianqiang@gmail.com>
2013-04-26 17:51:47 +0800

    426

--- app/controllers/money/moneyIncomeDetailAll.js
+++ app/controllers/money/moneyIncomeDetailAll.js
@@ -6,7 +6,7 @@ $.makeContextMenu = function(e, isSelectMode, sourceModel) {
 	var menuSection = Ti.UI.createTableViewSection();
 	menuSection.add($.createContextMenuItem("新增收入明细", function() {
 		Alloy.Globals.openWindow("money/moneyIncomeDetailForm", {selectedIncome : selectedIncome});
-	},!selectedExpense.canAddNew()));
+	},!selectedIncome.canAddNew()));
 	return menuSection;
 }
 
