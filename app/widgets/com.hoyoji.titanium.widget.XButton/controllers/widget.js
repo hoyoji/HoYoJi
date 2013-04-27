01dd1f435128f93a01c7cc4b36e831403e425a7c
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-04-27 15:32:34 +0800

    add xbutton

--- app/widgets/com.hoyoji.titanium.widget.XButton/controllers/widget.js
+++ app/widgets/com.hoyoji.titanium.widget.XButton/controllers/widget.js
@@ -1,1 +1,7 @@
 Alloy.Globals.extendsBaseUIController($, arguments[0]);
+
+$.button.addEventListener("singletap", function(e) {
+	$.trigger("singletap");
+	$.$view.fireEvent("singletap",{bubbles : true});
+	e.cancelBubble = true;
+});
