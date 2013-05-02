fa373e8334d60e118d0b284073fb06cf04dc1d81
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-05-02 16:53:29 +0800

    edit xButton

--- app/widgets/com.hoyoji.titanium.widget.XButton/controllers/widget.js
+++ app/widgets/com.hoyoji.titanium.widget.XButton/controllers/widget.js
@@ -1,5 +1,8 @@
 Alloy.Globals.extendsBaseUIController($, arguments[0]);
 
+if($.$attrs.backgroundImage){
+	$.imageView.setBackgroundImage($.$attrs.backgroundImage);
+}
 $.button.addEventListener("singletap", function(e) {
 	$.trigger("singletap");
 	$.$view.fireEvent("singletap",{bubbles : true});
