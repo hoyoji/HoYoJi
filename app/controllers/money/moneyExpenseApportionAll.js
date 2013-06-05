063c97d5ec13c27b4bb005915ed39fa40934a774
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-06-05 15:20:31 +0800

    a

--- app/controllers/money/moneyExpenseApportionAll.js
+++ app/controllers/money/moneyExpenseApportionAll.js
@@ -1,7 +1,10 @@
 Alloy.Globals.extendsBaseViewController($, arguments[0]);
 
+var selectedExpense = $.$attrs.selectedExpense;
+
 function onFooterbarTap(e){
 	if(e.source.id === "addExpenseDetail"){
 		Alloy.Globals.openWindow("money/moneyExpenseDetailForm",{selectedExpense : selectedExpense, closeWithoutSave : $.getCurrentWindow().$attrs.closeWithoutSave});
 	}
 }
+
