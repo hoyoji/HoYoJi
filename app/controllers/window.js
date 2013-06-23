221bd9771f8326278f68a7fa78e1679f08c15433
Ben Yiu <szepan.yiu@gmail.com>
2013-06-24 01:45:18 +0800

    c

--- app/controllers/window.js
+++ app/controllers/window.js
@@ -1,32 +1,46 @@
 Alloy.Globals.extendsBaseWindowController($, arguments[0]);
 
-exports.close = function(closeByUser) {
+function confirmClose() {
+	function doClose() {
+		$.$view.close({
+			animated : false
+		});
+	}
+
+	if (!$.getCurrentWindow().$attrs.closeWithoutSave && $.__dirtyCount > 0) {
+		Alloy.Globals.confirm("修改未保存", "你所做修改尚未保存，确认放弃修改并返回吗？", doClose, function() {
+			$.scrollableView.scrollToView(1);
+		});
+	} else {
+		doClose();
+	}
+}
+
+exports.close = function() {
 	//$.closeSoftKeyboard();
-	
-	function animateClose() {
-		if(closeByUser){
+
+	// function animateClose() {
 			$.scrollableView.scrollToView(0);
-		}
+		
 		// var animation = Titanium.UI.createAnimation();
 		// animation.left = "100%";
 		// animation.duration = 350;
 		// animation.curve = Titanium.UI.ANIMATION_CURVE_EASE_OUT;
 		// animation.addEventListener('complete', function() {
-			$.$view.close({
-				animated : false
-			});
+		// $.$view.close({
+		// animated : false
+		// });
 		// });
 		// $.$view.animate(animation);
-	}
-	
-
-	if (!$.getCurrentWindow().$attrs.closeWithoutSave && $.__dirtyCount > 0) {
-		Alloy.Globals.confirm("修改未保存", "你所做修改尚未保存，确认放弃修改并返回吗？", animateClose, function(){
-			$.scrollableView.scrollToView(1);
-		});
-	} else {
-		animateClose();
-	}
+	// }
+// 
+	// if (!$.getCurrentWindow().$attrs.closeWithoutSave && $.__dirtyCount > 0) {
+		// Alloy.Globals.confirm("修改未保存", "你所做修改尚未保存，确认放弃修改并返回吗？", animateClose, function() {
+			// $.scrollableView.scrollToView(1);
+		// });
+	// } else {
+		// animateClose();
+	// }
 }
 
 exports.open = function(contentController) {
@@ -36,32 +50,36 @@ exports.open = function(contentController) {
 	$.scrollableView.scrollToView(1);
 	//$.closeSoftKeyboard();
 	// if(OS_ANDROID){
-		// $.$view.focus();
+	// $.$view.focus();
 	// }
-	
+
 	// var animation = Titanium.UI.createAnimation();
 	// animation.left = "0";
 	// animation.duration = 350;
 	// animation.curve = Titanium.UI.ANIMATION_CURVE_EASE_OUT;
 	// if(contentController){
-		// animation.addEventListener("complete", function(){
+	// animation.addEventListener("complete", function(){
 	delete Alloy.Globals.openingWindow[contentController];
-		// });
+	// });
 	// }
 	// $.$view.animate(animation);
 }
 
 exports.openWin = function(contentController, options) {
 	options = options || {};
-	if(options.selectorCallback){
-		 _.extend(options, {height : "90%", width : "90%", borderRadius : 5});
+	if (options.selectorCallback) {
+		_.extend(options, {
+			height : "90%",
+			width : "90%",
+			borderRadius : 5
+		});
 	}
 
 	_.extend($.$attrs, options);
 	$.content = Alloy.createController(contentController, options);
 	$.content.setParent($.contentView);
 	// $.scrollableView.addView($.content.$view);
-	
+
 	$.open(contentController);
 	return $.content;
 }
@@ -76,16 +94,16 @@ exports.openWin = function(contentController, options) {
 // });
 
 // $.$view.addEventListener('swipe', function(e) {
-	// e.cancelBubble = true;
-	// if (e.direction === "right") {
-		// $.close();
-	// }
+// e.cancelBubble = true;
+// if (e.direction === "right") {
+// $.close();
+// }
 // });
 $.scrollableView.addEventListener("scrollend", function(e) {
 	if (e.source !== $.scrollableView) {
 		return;
 	}
-	if(e.currentPage === 0){
-		$.close(true);
+	if (e.currentPage === 0) {
+		confirmClose();
 	}
-});
+}); 
