be5b3ddfacfe417c15e50b5551ee3280ff313dc1
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-08-02 16:41:32 +0800

    a

--- app/models/MoneyAccount.js
+++ app/models/MoneyAccount.js
@@ -108,7 +108,7 @@ exports.definition = {
 			},
 			getCurrentBalance : function() {
 				if (this.xGet("ownerUser") === Alloy.Models.User) {
-					return this.xGet("currentBalance");
+					return Number(this.xGet("currentBalance").toFixed(2));
 				}
 			},
         	syncAddNew: function(record, dbTrans) {
