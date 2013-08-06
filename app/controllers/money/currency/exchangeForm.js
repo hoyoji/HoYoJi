a8e15f169d46fc60e26e3e5f6438f9104582cc5f
Ben Yiu <szepan.yiu@gmail.com>
2013-08-07 00:03:10 +0800

    c

--- app/controllers/money/currency/exchangeForm.js
+++ app/controllers/money/currency/exchangeForm.js
@@ -1,5 +1,27 @@
 Alloy.Globals.extendsBaseFormController($, arguments[0]);
 
+$.rate.rightButton.addEventListener("singletap", function(e) {
+	if(!$.$model.xGet("localCurrency")){
+		alert("请选择本币币种");
+		return;
+	}
+	if(!$.$model.xGet("foreignCurrency")){
+		alert("请选择外币币种");
+		return;
+	}
+	Alloy.Globals.Server.getExchangeRate(
+		$.$model.xGet("localCurrency").id,
+		$.$model.xGet("foreignCurrency").id,
+		function(rate){
+			$.rate.setValue(rate);
+			$.rate.field.fireEvent("change");
+		},
+		function(e){
+			alert(e);
+		}
+	);
+});
+
 $.localCurrency.UIInit($, $.getCurrentWindow());
 $.foreignCurrency.UIInit($, $.getCurrentWindow());
 $.rate.UIInit($, $.getCurrentWindow());
