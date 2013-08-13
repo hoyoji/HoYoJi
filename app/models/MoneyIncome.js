d21a30cad3a476558cb1957b1eed724ddd52214f
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-08-13 13:37:41 +0800

    q

--- app/models/MoneyIncome.js
+++ app/models/MoneyIncome.js
@@ -158,12 +158,16 @@ exports.definition = {
 				}
 			},
 			getLocalAmount : function() {
-				var projectCurrency = this.xGet("project").xGet("currency");
-				var userCurrency = Alloy.Models.User.xGet("activeCurrency");
-				var exchanges = userCurrency.getExchanges(projectCurrency);
 				var exchange = 1;
-				if (exchanges.length) {
-					exchange = exchanges.at(0).xGet("rate");
+				if (this.xGet("ownerUser") === Alloy.Models.User && this.xGet("moneyAccount").xGet("currency") === Alloy.Models.User.xGet("activeCurrency")) {
+					exchange = this.xGet("exchangeRate");
+				} else {
+					var projectCurrency = this.xGet("project").xGet("currency");
+					var userCurrency = Alloy.Models.User.xGet("activeCurrency");
+					var exchanges = userCurrency.getExchanges(projectCurrency);
+					if (exchanges.length) {
+						exchange = exchanges.at(0).xGet("rate");
+					}
 				}
 				return Alloy.Models.User.xGet("activeCurrency").xGet("symbol") + (this.xGet("amount") * this.xGet("exchangeRate") / exchange).toFixed(2);
 			},
