8fd263059f5c548bf85a2504b5f7fbe997889935
SZE PAN BEN YIU <szepanbenyiu@SZEtekiMacBook-Pro.local>
2013-08-16 14:35:11 +0800

    c

--- app/controllers/user/userForm.js
+++ app/controllers/user/userForm.js
@@ -1,7 +1,15 @@
 Alloy.Globals.extendsBaseFormController($, arguments[0]);
 
 $.onSave = function(saveEndCB, saveErrorCB) {
-	Alloy.Models.User.save({newFriendAuthentication : "none" }, {wait : true, patch : true, success : saveEndCB});
+	Alloy.Models.User.save({
+		newFriendAuthentication : "none"
+	}, {
+		wait : true,
+		patch : true,
+		success : saveEndCB
+	});
 }
 
 $.newFriendAuthentication.UIInit($, $.getCurrentWindow());
+
+
