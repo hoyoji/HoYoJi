373e42c970bc54305c0a26fe88698f1dea1bb1cd
SZE PAN BEN YIU <szepanbenyiu@SZEtekiMacBook-Pro.local>
2013-08-16 14:59:33 +0800

    c

---
+++ app/controllers/user/login.js
@@ -0,0 +1,198 @@
+Alloy.Globals.extendsBaseFormController($, arguments[0]);
+
+$.setSaveableMode("add");
+
+function doLogin(e) {
+	delete Alloy.Models.User;
+	delete Alloy.Globals.currentUserDatabaseName;
+
+	// make new login ID every time user tries to login
+	// delete $.$model.id;
+	// $.$model = Alloy.createModel("Login", {
+	// ownerUser : Alloy.Models.User
+	// });
+	// // $.$model.attributes.id = guid();
+	// $.$model.xSet("date", (new Date()).toISOString());
+
+	// if (!$.$model.xGet("password")) {
+	// $.password.showErrorMsg("请输入密码");
+	// return;
+	// }
+	// encrypt the password
+	// $.$model.xSet("password", Ti.Utils.sha1($.$model.xGet("password")));
+	var userName = Alloy.Globals.alloyString.trim($.userName.field.getValue()) || "";
+	if (userName.length === 0) {
+		$.userName.showErrorMsg("请输入用户名");
+		return;
+	}
+	var password = $.password.field.getValue() || "";
+	if (password.length === 0) {
+		$.password.showErrorMsg("请输入密码");
+		return;
+	}
+	password = Ti.Utils.sha1(password);
+	var userDatabase = Alloy.createModel("UserDatabase");
+	userDatabase.fetch({
+		query : "SELECT * FROM UserDatabase WHERE userName = '" + userName +"'"
+	});
+	if (!userDatabase.id) {
+		// 本地没有该用户的数据库，我们到服务器上验证该用户
+		//用户不存在，到服务器上下载用户资料
+			Alloy.Globals.Server.postData({
+				userName : userName,
+				password : password
+			}, function(data) {
+				userDatabase.set({id : data.id, "userName" : userName}, {patch : true});
+				userDatabase.save();
+				Alloy.Globals.currentUserDatabaseName = data.id;
+				loginUser();
+			}, function(e) {
+				// 用户验证错误或无法连接服务器，登录失败
+				loginFail(e.__summary.msg);
+			}, "login");	
+	} else {
+		Alloy.Globals.currentUserDatabaseName = userDatabase.id;
+		loginUser();
+	}
+	
+	function loginUser() {
+		Alloy.Globals.DataStore.initStore();
+		
+		Alloy.Models.User = Alloy.createModel("User").xFindInDb({
+			userName : userName
+		});
+
+		// 如果我们能在本地找到该用户， 我们先检查该用户的密码正不正确， 如果密码不正确，我们则到服务器上验证
+		if (Alloy.Models.User.id) {
+			// $.$model.xSet("ownerUser", Alloy.Models.User);
+			// $.saveModel();
+			if (Alloy.Models.User.xGet("password") === password) {
+				openMainWindow();
+			} else {
+				// 密码不对，到服务器上验证密码
+				// 这里最好是用户连续输错三次密码才到服务器上验证，并且要征求用户意见
+				Alloy.Globals.Server.postData({
+					userName : userName,
+					password : password
+				}, function(data) {
+					// 服务器验证改密码正确，我们将其保存到本地
+					Alloy.Models.User.save({
+						"password" : password
+					}, {
+						patch : true,
+						wait : true
+					});
+					openMainWindow();
+				}, function(e) {
+					// 服务器无法连接，或验证该密码错误，用户登录失败
+					loginFail(e.__summary.msg);
+				}, "login");
+			}
+		} else {
+			//用户不存在，到服务器上下载用户资料
+			Alloy.Globals.Server.postData({
+				userName : userName,
+				password : password
+			}, function(data) {
+				// 密码验证通过，将该用户的资料保存到本地数据库
+				data.password = password;
+				// 由于服务器不会反回密码，我们将用户输入的正确密码保存
+				delete data.lastSyncTime;
+				Alloy.Models.User.set(data);
+				delete Alloy.Models.User.id;
+				// 将用户id删除，我们才能将该用户资料当成新的记录保存到数据库
+				Alloy.Models.User.xAddToSave($);
+
+				// 下载一些用户必须的资料
+				var belongsToes = [];
+				for (var belongsTo in Alloy.Models.User.config.belongsTo) {
+					if (Alloy.Models.User.xGet(belongsTo + "Id")) {
+						belongsToes.push({
+							id : Alloy.Models.User.xGet(belongsTo + "Id"),
+							__dataType : Alloy.Models.User.config.belongsTo[belongsTo].type
+						})
+						if (belongsTo === "activeProject") {
+							belongsToes.push({
+								friendUserId : Alloy.Models.User.xGet("id"),
+								projectId : Alloy.Models.User.xGet(belongsTo + "Id"),
+								__dataType : "ProjectShareAuthorization"
+							});
+							belongsToes.push({
+								// parentExpenseCategoryId : null,
+								projectId : Alloy.Models.User.xGet(belongsTo + "Id"),
+								__dataType : "MoneyExpenseCategory"
+							});
+							belongsToes.push({
+								// parentIncomeCategoryId : null,
+								projectId : Alloy.Models.User.xGet(belongsTo + "Id"),
+								__dataType : "MoneyIncomeCategory"
+							});							
+						}
+					}
+				}
+				Alloy.Globals.Server.getData(belongsToes, function(data) {
+					// 将数据保存到本地数据库
+					data = _.flatten(data);
+					data.forEach(function(model) {
+						var modelType = model.__dataType, id = model.id;
+						delete model.id;
+						delete model.__dataType;
+						// 把默认账户余额设成0，因为我们还没下载任何账务资料
+						if (modelType === "MoneyAccount") {
+							model.currentBalance = 0;
+						}
+						model = Alloy.createModel(modelType, model);
+						model.attributes["id"] = id;
+						model.xAddToSave($);
+					});
+					// $.$model.xSet("ownerUser", Alloy.Models.User);
+					Alloy.Models.User.xSet("password", $.password.getValue());
+					$.saveCollection(function() {
+						openMainWindow();
+					}, function(e) {
+						// 保存倒数据库时出错，登录失败
+						loginFail(e);
+					});
+				}, function(e) {
+					// 无法连接服务器，登录失败
+					loginFail(e.__summary.msg);
+				});
+			}, function(e) {
+				// 用户验证错误或无法连接服务器，登录失败
+				loginFail(e.__summary.msg);
+			}, "login");
+		}
+	}
+
+}
+
+function openMainWindow() {
+	$.password.field.setValue("");
+	// $.$model.xSet("password", null);
+	if (!Alloy.Globals.mainWindow) {
+		var win = Alloy.createController("mainWindow", {
+			autoInit : "false"
+		});
+		win.UIInit();
+		win.open();
+	}
+}
+
+function loginFail(msg) {
+	$.password.field.setValue("");
+	// $.$model.xSet("password", null);
+	// $.$model.xSet("ownerUser", null);
+	Alloy.Models.User = null;
+	delete Alloy.Models.User;
+	alert(msg);
+}
+
+function openRegister(e) {
+	Alloy.Globals.openWindow("user/registerForm", {
+		$model : "User"
+	});
+}
+
+$.userName.UIInit($, $.getCurrentWindow());
+$.password.UIInit($, $.getCurrentWindow());
+
---
+++ app/controllers/user/registerForm.js
@@ -0,0 +1,122 @@
+Alloy.Globals.extendsBaseFormController($, arguments[0]);
+
+// var activeCurrency = Alloy.createModel("Currency", {name : "人民币", symbol : "￥", code : "CNY", ownerUser : $.$model}).xAddToSave($);
+// activeCurrency.attributes["id"] = "CNY";
+//
+// $.$model.xSet("activeCurrency", activeCurrency);
+//
+// var activeProject = Alloy.createModel("Project", {name : "我的收支", currencyId : "CNY", ownerUser : $.$model}).xAddToSave($);
+// $.$model.xSet("activeProject", activeProject);
+//
+// Alloy.createModel("ProjectShareAuthorization", {
+// project : activeProject,
+// state : "Accept",
+// friendUser : $.$model,
+// ownerUser : $.$model
+// }).xAddToSave($);
+//
+// var defaultFriendCategory = Alloy.createModel("FriendCategory", {name : "我的好友", ownerUser : $.$model}).xAddToSave($);
+// $.$model.xSet("defaultFriendCategory", defaultFriendCategory);
+//
+// //var merchantFriendCategory = Alloy.createModel("FriendCategory", {name : "商家好友", ownerUser : $.$model}).xAddToSave($);
+//
+// var messageBox = Alloy.createModel("MessageBox", {ownerUser : $.$model}).xAddToSave($);
+// $.$model.xSet("messageBox", messageBox);
+//
+//
+// var activeMoneyAccount = Alloy.createModel("MoneyAccount", {name : "现金", currencyId : "CNY", currentBalance : 0, sharingType : "Private", accountType : "Cash",ownerUser : $.$model}).xAddToSave($);
+// $.$model.xSet("activeMoneyAccount", activeMoneyAccount);
+//
+// var defaultIncomeCategory = Alloy.createModel("MoneyIncomeCategory",{name : "日常收入", project:$.$model.xGet("activeProject"), ownerUser : $.$model}).xAddToSave($);
+// $.$model.xGet("activeProject").xSet("defaultIncomeCategory",defaultIncomeCategory);
+//
+// var defaultExpenseCategory = Alloy.createModel("MoneyExpenseCategory",{name : "日常支出", project:$.$model.xGet("activeProject"), ownerUser : $.$model}).xAddToSave($);
+// $.$model.xGet("activeProject").xSet("defaultExpenseCategory",defaultExpenseCategory);
+//
+// var depositeIncomeCategory = Alloy.createModel("MoneyIncomeCategory",{name : "充值收入", project:$.$model.xGet("activeProject"), ownerUser : $.$model}).xAddToSave($);
+// $.$model.xGet("activeProject").xSet("depositeIncomeCategory",depositeIncomeCategory);
+//
+// var depositeExpenseCategory = Alloy.createModel("MoneyExpenseCategory",{name : "充值支出", project:$.$model.xGet("activeProject"), ownerUser : $.$model}).xAddToSave($);
+// $.$model.xGet("activeProject").xSet("depositeExpenseCategory",depositeExpenseCategory);
+
+$.onSave = function(saveEndCB, saveErrorCB) {
+	// $.$model.xValidate(function() {
+	// // 先在本对用户资料进行验证, 如果验证通过，则到服务器上注册
+	// if ($.$model.__xValidationErrorCount > 0) {
+	// $.$model.__xValidationError.__summary = {
+	// msg : "验证错误"
+	// };
+	// for (var e in $.$model.__xValidationError) {
+	// console.info(e + " : " + $.$model.__xValidationError[e].msg);
+	// }
+	// $.$model.trigger("error", $.$model, $.$model.__xValidationError);
+	// saveErrorCB($.$model.__xValidationError.__summary.msg);
+	// } else {
+	// // 把用户注册时创建的资料发到服务器上进行注册
+	// var userData = $.$model.toJSON();
+	// userData.password = Ti.Utils.sha1($.$model.xGet("password"));
+	// var data = [userData];
+	// for (var i = 0; i < $.__saveCollection.length; i++) {
+	// data.push($.__saveCollection[i].toJSON());
+	// }
+	$.$model.xValidate(function() {
+		// 先在本对用户资料进行验证, 如果验证通过，则到服务器上注册
+		if ($.$model.__xValidationErrorCount > 0) {
+			$.$model.trigger("error", $.$model, $.$model.__xValidationError);
+		}
+	});
+	if (Alloy.Globals.alloyString.trim($.$model.xGet("userName")).length === 0) {
+		saveErrorCB("验证错误");
+		return;
+	}
+	if (!$.$model.xGet("password")) {
+		saveErrorCB("验证错误");
+		return;
+	}
+	if (!$.$model.xGet("password2")) {
+		saveErrorCB("验证错误");
+		return;
+	}
+	if ($.$model.xGet("password") !== $.$model.xGet("password2")) {
+		saveErrorCB("验证错误");
+		return;
+	}
+	var currencyId = Ti.Locale.getCurrencyCode(Ti.Locale.getCurrentLocale());
+	var data = {
+		userName : Alloy.Globals.alloyString.trim($.$model.xGet("userName")),
+		password : Ti.Utils.sha1($.$model.xGet("password")),
+		currencyId : currencyId,
+		currencySymbol : Ti.Locale.getCurrencySymbol(currencyId)
+	};
+	Alloy.Globals.Server.postData(data, function(returnedData) {
+		// $.$model.xSet("lastSyncTime", returnedData.lastSyncTime);
+		// $.saveModel(function(msg){
+		// Alloy.Models.User = $.$model;
+		saveEndCB("注册成功");
+		$.getCurrentWindow().__dirtyCount = 0;
+		$.getCurrentWindow().close();
+		// }, saveErrorCB);
+	}, function(e) {
+		// 连接服务器出错或用户名已经存在，注册不成功
+		$.$model.__xValidationErrorCount = 1;
+		$.$model.__xValidationError = e;
+		$.$model.trigger("error", $.$model, $.$model.__xValidationError);
+		//
+		// $.password.field.setValue("");
+		// $.password2.field.setValue("");
+		// $.$model.xSet("password", null);
+		// $.$model.xSet("password2", null);
+		saveErrorCB(e.__summary.msg);
+	}, "registerUser");
+	// }
+	// });
+}
+
+$.onWindowCloseDo(function() {
+	$.$model = null;
+	// Alloy.Globals.DataStore.initStore();
+})
+
+$.userName.UIInit($, $.getCurrentWindow());
+$.password.UIInit($, $.getCurrentWindow());
+$.password2.UIInit($, $.getCurrentWindow());
