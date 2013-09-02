3c869886817fdf79d70bda2109fe23e6ff212c57
bruselee <hoyoji.lijianqiang@gmail.com>
2013-09-02 20:42:20 +0800

    p

--- app/controllers/money/moneyIncomeRow.js
+++ app/controllers/money/moneyIncomeRow.js
@@ -116,7 +116,7 @@ $.makeContextMenu = function() {
 					"messageState" : "new",
 					"messageTitle" : "删除充值",
 					"date" : date,
-					"detail" : "用户" + Alloy.Models.User.xGet("userName") + "删除了充值收入",
+					"detail" : "用户" + Alloy.Models.User.xGet("userName") + "请求删除充值",
 					"messageBoxId" : $.$model.xGet("friendUser").xGet("messageBoxId"),
 					messageData : JSON.stringify({
 						accountType : "MoneyIncome",
