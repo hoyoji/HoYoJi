385d65a11a66bff6601c9c24c983782e60063762
unknown <hoyoji@.(none)>
2013-09-06 23:29:20 +0800

    fix #791

--- app/models/MoneyIncomeApportion.js
+++ app/models/MoneyIncomeApportion.js
@@ -191,18 +191,24 @@ exports.definition = {
 					// patch : true,
 					// syncFromServer : true
 				// };
-				var self = this;
-				var projectShareAuthorizations = self.xGet("moneyIncome").xGet("project").xGet("projectShareAuthorizations");
-				projectShareAuthorizations.forEach(function(projectShareAuthorization) {
-					if (projectShareAuthorization.xGet("friendUser") === self.xGet("friendUser")) {
+				// var self = this;
+				// var projectShareAuthorizations = self.xGet("moneyIncome").xGet("project").xGet("projectShareAuthorizations");
+				// projectShareAuthorizations.forEach(function(projectShareAuthorization) {
+					// if (projectShareAuthorization.xGet("friendUser") === self.xGet("friendUser")) {
 						// var apportionedTotalIncome = projectShareAuthorization.xGet("apportionedTotalIncome") || 0;
 						// projectShareAuthorization.xSet("apportionedTotalIncome", apportionedTotalIncome - self.xGet("amount")*self.xGet("moneyIncome").xGet("exchangeRate"));
 						// projectShareAuthorization.save({
 							// apportionedTotalIncome : apportionedTotalIncome - Number((self.xGet("amount") * self.xGet("moneyIncome").xGet("exchangeRate")).toFixed(2))
 						// }, saveOptions);
-						projectShareAuthorization.__syncApportionedTotalIncome = projectShareAuthorization.__syncApportionedTotalIncome ? projectShareAuthorization.__syncApportionedTotalIncome - Number((self.xGet("amount") * self.xGet("moneyIncome").xGet("exchangeRate")).toFixed(2)) : - Number((self.xGet("amount") * self.xGet("moneyIncome").xGet("exchangeRate")).toFixed(2));
-					}
+								var projectShareAuthorization = Alloy.createModel("ProjectShareAuthorization").xFindInDb({
+					projectId : record.projectId,
+					friendUserId : record.friendUserId
 				});
+				if (projectShareAuthorization.id) {
+						projectShareAuthorization.__syncApportionedTotalIncome = projectShareAuthorization.__syncApportionedTotalIncome ? projectShareAuthorization.__syncApportionedTotalIncome - Number((self.xGet("amount") * self.xGet("moneyIncome").xGet("exchangeRate")).toFixed(2)) : - Number((self.xGet("amount") * self.xGet("moneyIncome").xGet("exchangeRate")).toFixed(2));
+				}
+					// }
+				// });
 			}
 		});
 		return Model;
