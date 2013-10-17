056d04a237f1c6cb6e41f38daa3b7ede23acae44
hoyoji <hoyoji@hoyojitekiMacBook-Air.local>
2013-10-17 09:49:01 +0800

    e

--- app/controllers/user/findPassword.js
+++ app/controllers/user/findPassword.js
@@ -7,7 +7,23 @@ var onFooterbarTap = function(e) {
 };
 
 function sendEmail(){
-	
+	if (!$.email.getValue()) {
+		alert("请输入邮箱");
+	} else {
+		var emailValidation = /^([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/;
+		if (!emailValidation.test($.email.getValue())) {
+			alert("email不正确");
+			$.email.setValue("");
+		} else {
+			Alloy.Globals.Server.postData([{
+				email : Alloy.Globals.alloyString.trim($.email.getValue())
+			}], function(data) {
+				alert(data.msg);
+			}, function(e) {
+				alert(e.__summary.msg);
+			}, "passwordRecoverEmail");
+		}
+	}
 }
 
 $.titleBar.UIInit($, $.getCurrentWindow());
