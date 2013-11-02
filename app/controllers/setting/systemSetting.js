835b797fecfd0653a1753763d03339bd40b38581
admin <admin@Mac.local>
2013-11-02 20:35:36 +0800

    c

--- app/controllers/setting/systemSetting.js
+++ app/controllers/setting/systemSetting.js
@@ -18,13 +18,13 @@ $.logout.addEventListener("singletap", function(e) {
 	e.cancelBubble = true;
 });
 
-$.userView.addEventListener("singletap", function(e) {
-	if (e.source !== $.logout) {
-		Alloy.Globals.openWindow("user/huserDetail", {
-			$model : Alloy.Models.User
-		});
-	}
-});
+// $.userView.addEventListener("singletap", function(e) {
+	// if (e.source !== $.logout) {
+		// Alloy.Globals.openWindow("user/huserDetail", {
+			// $model : Alloy.Models.User
+		// });
+	// }
+// });
 
 $.onWindowOpenDo(function() {
 	$.currentVersion.addEventListener("click", openAbout);
@@ -36,8 +36,37 @@ $.onWindowCloseDo(function() {
 
 $.currentVersionNumber.setText(Ti.App.version);
 
-$.userPicture.UIInit($, $.getCurrentWindow());
+
+$.makeContextMenu = function() {
+	var menuSection = Ti.UI.createTableViewSection({
+		headerTitle : "用户操作"
+	});
+	menuSection.add($.createContextMenuItem("导入用户图片", function() {
+		$.picture.importPictureFromGallery();
+	}));
+	return menuSection;
+};
+
+function changePassword() {
+	Alloy.Globals.openWindow("user/changePasswordForm", {
+		currentUser : Alloy.Models.User
+	});
+}
+
+$.email.rightButton.addEventListener("singletap", function(){
+	alert("很抱歉，验证功能尚未完成！");
+});
+$.changePassword.addEventListener("singletap", changePassword);
+
+$.picture.UIInit($, $.getCurrentWindow());
+$.nickName.UIInit($, $.getCurrentWindow());
 $.userName.UIInit($, $.getCurrentWindow());
+$.email.UIInit($, $.getCurrentWindow());
+// $.changePassword.UIInit($, $.getCurrentWindow());
+$.titleBar.UIInit($, $.getCurrentWindow());
+
+// $.userPicture.UIInit($, $.getCurrentWindow());
+// $.userName.UIInit($, $.getCurrentWindow());
 $.friendAuthentication.UIInit($, $.getCurrentWindow());
 $.titleBar.UIInit($, $.getCurrentWindow());
 
--- app/controllers/user/huserDetail.js
+++ app/controllers/user/huserDetail.js
@@ -4,7 +4,7 @@ $.makeContextMenu = function() {
 	var menuSection = Ti.UI.createTableViewSection({
 		headerTitle : "用户操作"
 	});
-	menuSection.add($.createContextMenuItem("导入图片", function() {
+	menuSection.add($.createContextMenuItem("导入用户图片", function() {
 		$.picture.importPictureFromGallery();
 	}));
 	return menuSection;
