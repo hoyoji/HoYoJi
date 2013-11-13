f0705ee37cac029109cf9f25f3b56a306232c95d
zhangzanfu <hoyoji.zhangzanfu@gmail.com>
2013-11-13 11:40:17 +0800

    a

--- app/controllers/user/changePasswordForm.js
+++ app/controllers/user/changePasswordForm.js
@@ -13,11 +13,25 @@ function updatePassword() {
 		$.oldPassword.showErrorMsg("当前密码长度不对");
 		return;
 	}
+	
 	var newPassword = $.newPassword.field.getValue() || "";
 	if (!passwordValidation.test(newPassword)) {
 		$.newPassword.showErrorMsg("新密码长度不对");
 		return;
+	}else {
+		var repeat = true;
+		var series = true;
+		var first = newPassword.charAt(0);
+		for (var i = 1; i < newPassword.length; i++) {
+			repeat = repeat && newPassword.charAt(i) === first;
+			series = series && newPassword.charCodeAt(i) === newPassword.charCodeAt(i - 1) + 1;
+		}
+		if (repeat || series) {
+			$.newPassword.showErrorMsg("新密码太简单");
+			return;
+		}
 	}
+	
 	if(newPassword === oldPassword){
 		$.newPassword.showErrorMsg("新密码和当前密码相同");
 		return;
