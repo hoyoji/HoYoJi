6585d3159e36b6916acb5e68134e83859e669780
admin <admin@Mac.local>
2013-11-23 10:47:56 +0800

    c

--- app/models/UserData.js
+++ app/models/UserData.js
@@ -16,6 +16,7 @@ exports.definition = {
 			newFriendAuthentication : "TEXT NOT NULL",
 			defaultFriendCategoryId : "TEXT NOT NULL",
 			messageBoxId : "TEXT NOT NULL",
+			moneyDebtAccountId : "TEXT NOT NULL",
 			//isMerchant : "INTEGER NOT NULL",
 			serverRecordHash : "TEXT",
 			lastServerUpdateTime : "TEXT",
